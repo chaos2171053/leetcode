c1fbac67eb96925e24ba102b895e88624827f10d
chaos2171053 <chaos2171053@gmail.com>
2020-09-21 22:55:31 +0800

    [ADD]strStr

---
+++ 28.实现-str-str.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=javascript
+ *
+ * [28] 实现 strStr()
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    if (!needle.length) {
+        return 0;
+    }
+    for (let i = 0, len = haystack.length; i < len; i++) {
+
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i
+        }
+    }
+    return -1
+};
+
+// @lc code=end
+
